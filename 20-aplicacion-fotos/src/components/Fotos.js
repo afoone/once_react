9b7bce60a156d9c2043c2e7954ae233921ab0bd5
Alfonso Tienda <atienda@iprocuratio.com>
2020-04-21 15:04:00 +0200

    improved

--- 20-aplicacion-fotos/src/components/Fotos.js
+++ 20-aplicacion-fotos/src/components/Fotos.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import './Fotos.css'
 
 export class Fotos extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            fotos: [],
+          //  pagina: 
+        }
+    }
+
 
     componentDidMount() {
-        axios.get("https://api.unsplash.com/search/photos?page=1&query=valencia&client_id=key&client_secret=secret").then(
-            res => console.log(res)
+        axios.get("https://api.unsplash.com/search/photos?page=1&query=coronavirus&client_id=ap9CjpGv1KQL1_NUBugcP7F1JuMxCSDt9SeyPL9hGSc&client_secret=wujp4hUZplKTxdMIeeCzj8r0IUtKWgKmfRY-QJIU7SU").then(
+            res => this.setState(
+                {
+                    fotos: res.data.results
+                }
+            )
+
         )
     }
 
 
     render() {
+        console.log(this.state.fotos)
         return (
-            <div>
-                
-            </div>
+            <>
+                <div className="fotos">
+                    {
+                        this.state.fotos.map(
+                            e => {
+                                const apaisada = e.height < e.width;
+                                const estilo = {
+                                    gridColumn: `span ${apaisada?2:1}`
+                                }
+                                return <img alt={e.alt_description} key={e.id} src={e.urls.small} style={estilo}></img>
+                            }
+                        )
+                    }
+                </div>
+                <button>Más</button>
+            </>
         )
     }
 }
