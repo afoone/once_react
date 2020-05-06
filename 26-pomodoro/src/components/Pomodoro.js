54324724e2af8d0d61c2b333dd12285dcabb3ba8
Alfonso Tienda <atienda@iprocuratio.com>
2020-05-06 15:32:24 +0200

    pomodoro ii

--- 26-pomodoro/src/components/Pomodoro.js
+++ 26-pomodoro/src/components/Pomodoro.js
@@ -2,33 +2,74 @@ import React, { Component } from 'react'
 import Header from './Header'
 import './Pomodoro.css'
 import Mensaje from './Mensaje';
+import Sound from 'react-sound'
 
 export class Pomodoro extends Component {
+
+
+    // 25 mt - 5md - 25mt - 5md - 25mt - 5m - 25mt - 15md
+
+    configuracion = {
+        descansoLargo: 900,
+        descansoCorto: 300,
+        trabajo: 1500,
+        cortoPorCadaLargo: 3
+    }
+
 
     constructor(props) {
         super(props);
         this.state = {
-            tiempo: 1500,
+            tiempo: this.configuracion.trabajo,
             running: false,
-            mensaje: null
+            working: true,
+            descansosCortos: 0,
+            mensaje: null,
+            timeRunning: this.configuracion.trabajo,
+            play: false
         }
         this.interval = null;
     }
 
-    // componentDidMount() {
-    // }
+    _nextDescanso() {
+        if (this.state.descansosCortos === this.configuracion.cortoPorCadaLargo - 1) {
+            return {
+                descansosCortos: 0,
+                tiempo: this.configuracion.descansoLargo,
+            }
+        } else {
+            return {
+                descansosCortos: this.state.descansosCortos + 1,
+                tiempo: this.configuracion.descansoCorto
+            }
+        }
+    }
+
+    timeFinished = () => {
+        const descanso = this._nextDescanso();
+        console.log(descanso);
+        this.setState(
+            {
+                tiempo: this.state.working ?
+                    descanso.tiempo : this.configuracion.trabajo,
+                working: !this.state.working,
+                descansosCortos: !this.state.working ? descanso.descansosCortos 
+                    : this.state.descansosCortos,
+                running: false,
+                mensaje: this.state.working ? "Descansando" : "Trabajando",
+                timeRunning: this.state.working ?
+                    this.configuracion.descansoCorto : this.configuracion.trabajo,
+                play: true
+            }
+        )
+    }
 
     onStart = () => {
         this.interval = setInterval(
             () => {
                 if (this.state.tiempo === 0) {
-                    clearInterval(this.interval);
-                    this.setState(
-                        {
-                            running: false,
-                            mensaje: "El tiempo ha terminado"
-                        }
-                    )
+                    //clearInterval(this.interval);
+                    this.timeFinished();
                 } else {
                     this.setState(
                         {
@@ -38,7 +79,7 @@ export class Pomodoro extends Component {
                     )
                 }
             },
-            10
+            1000
         )
     }
 
@@ -49,24 +90,34 @@ export class Pomodoro extends Component {
                 running: false
             }
         )
-
     }
 
     onReset = () => {
         clearInterval(this.interval)
         this.setState(
             {
-                tiempo: 1500,
+                tiempo: this.configuracion.trabajo,
                 running: false,
                 mensaje: null
             }
         )
     }
 
+    handleSongFinishedPlaying = () => {
+        this.setState(
+            {play: false}
+        )
+        console.log("finish play")
+    }
+
     render() {
         return (
             <div className="pomodoro" >
                 <Header></Header>
+                <Sound url="/ding.wav"
+                                        playStatus={Sound.status.PLAYING}
+                                        
+                                    ></Sound>
                 <div className="ui cards">
                     <div className="card">
                         <div className="content">
@@ -74,16 +125,26 @@ export class Pomodoro extends Component {
                                 {Math.floor(this.state.tiempo / 60)}:
         {this.state.tiempo % 60 < 10 ? "0" : null}{this.state.tiempo % 60}
                             </div>
+
+                            {/* <div>Descansos cortos: {this.state.descansosCortos}</div> */}
+
                             {this.state.mensaje ?
-                                 <Mensaje mensaje={this.state.mensaje}></Mensaje> 
-                                 : null}
+                                <>
+                                    <Mensaje mensaje={this.state.mensaje}></Mensaje>
+                                    <Sound url="/ding.wav"
+                                        playStatus={this.state.play ? Sound.status.PLAYING : Sound.status.STOPPED}
+                                        onFinishedPlaying={this.handleSongFinishedPlaying}
+                                    ></Sound>
+                                </>
+                                : null}
                         </div>
-                        <div class="ui progress">
-  <div class="bar" style={{width: "70%"}}>
-    <div class="progress" ></div>
-  </div>
-  <div class="label">Uploading Files</div>
-</div>
+                        <div className="ui red progress">
+                            <div className="bar"
+                                style={{ width: (100 - (Math.floor(this.state.tiempo * 100 / this.state.timeRunning))) + "%" }} >
+                                <div className="progress" ></div>
+                            </div>
+                            {/* <div class="label">Uploading Files</div> */}
+                        </div>
                         <div className="extra content">
                             <div className="ui three buttons">
                                 <div className={`ui basic green button ${this.state.running ? "disabled" : ""}`}
